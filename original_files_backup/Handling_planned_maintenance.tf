91024c904284410898287ac87fb248acace66dc5
NoahInCloud <Noahkanga@gmail.com>
2025-03-02 17:22:15 +0100

    AWS Devops with Terraform

--- original_files_backup/Handling_planned_maintenance.tf
+++
@@ -1,9 +0,0 @@
-resource "null_resource" "perform_maintenance" {
-  triggers = {
-    vm_id = aws_instance.vm.id
-  }
-
-  provisioner "local-exec" {
-    command = "aws ec2 reboot-instances --instance-ids ${aws_instance.vm.id}"
-  }
-}
